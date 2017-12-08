ba05a8975aab5d1075a6e622a3cfdcfa4dc5f69f
Avi Sriram <33232059+TheMeapster@users.noreply.github.com>
2017-12-08 14:02:42 -0500

    Update list3.py

--- list3.py
+++ list3.py
@@ -2,19 +2,21 @@
 #Dylan and Avi
 #12/8/17
 
-from random import randint
+from random import *
 
 def main():
+print ("This is will make a random list of number between 1 and 50 and it will see if 27 is in the list and it will then tell you the frequency of 27.")
     count = 0
     nums = []
-    for i in range(50):
-        num = randint(1, 50)
+    for i in range (50):
+        num = randint (1, 50)
         nums.append(num)
     for i in nums:
         if i == 27:
             count = count + 1
-    nums.sort()
-    print "The number 27 is in the list", count, "times."
-    print "The list is:", nums
+        nums.sort()
+            print "27 is in the list and it repeats",count,"times"
+            print "The list is", nums
+
 
 main()
