47bc3003d683c3b0b9def04f908080c07aaaacbb
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-12-08 14:22:48 -0500

    Create list4.py

---
+++ list4.py
@@ -0,0 +1,15 @@
+#List-3
+#Dylan and Avi
+#12/8/17
+
+from random import randint
+
+def main():
+    print "This will make a list of 101 random numbers from 1-1000 and it will find the median of the list."
+    nums = []
+    for i in range(101):
+        num = randint(1, 1000)
+        nums.append(num)
+    print "The median of the list is:", nums[51]
+
+main()
