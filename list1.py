cb7ad1e188c8a244aa7616b95607c19711a01c93
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-12-08 13:34:55 -0500

    Update list1.py

--- list1.py
+++ list1.py
@@ -1,1 +1,24 @@
+#List-1
+#DM and AS
+#12/8/17
 
+def main():
+    print "You will be asked to input 10 numbers and the average of those numbers will be calculated and displayed."
+    nums = []
+    tot = 0
+    for i in range(10):
+        digit = 0
+        while digit == 0:
+            num = raw_input("Enter a number to add it to the list: ")
+            if num.isdigit():
+                num = int(num)
+                nums.append(num)
+                digit = 1
+            else:
+                print "That is not a number."
+    for i in nums:
+        tot = tot + i
+    ave = tot/10.00000000000000000000000000000000000000000
+    print "The average is:", ave
+
+main()
