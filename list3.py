b77617ef49364fea74a2511f8fad9622c402b30f
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-12-08 14:00:03 -0500

    Update list3.py

--- list3.py
+++ list3.py
@@ -1,1 +1,20 @@
+#List-3
+#Dylan and Avi
+#12/8/17
 
+from random import randint
+
+def main():
+    count = 0
+    nums = []
+    for i in range(50):
+        num = randint(1, 50)
+        nums.append(num)
+    for i in nums:
+        if i == 27:
+            count = count + 1
+    nums.sort()
+    print "The number 27 is in the list", count, "times."
+    print "The list is:", nums
+
+main()
