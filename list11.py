75ac96c9f75f9fe6c9534d181e16370633095839
Dylan <dylan@Family-MacBook-Air.local>
2017-12-15 16:06:44 -0500

    Almost Done

--- list11.py
+++ list11.py
@@ -4,22 +4,36 @@
 def validInput():
     valid = 1
     while valid == 1:
-        string1 = raw_input("Enter a string of just letters: ")
-        string2 = raw_input("Enter another string letters: ")
-        for i in string1:
-            if i.isalpha:
-                list1.append(i)
-            else:
-                print "That is not a valid string."
-                return
+        string = raw_input("Enter a string of just letters: ")
+        valid2 = 1
+        while valid2 == 1:
+            for i in string:
+                print i
+                if i.isalpha:
+                    if i == string[-1]:
+                        return string
+                else:
+                    print "That is not a valid string."
+                    valid2 = 0
+                    continue
+
 def main():
-    list1 = validInput()
-    count = 0
+    string1 = validInput()
+    string2 = validInput()
+    list1 = []
+    list2 = []
     for i in string1:
-        if i in string2:
+        list1.append(i)
+    for i in string2:
+        list2.append(i)
+    count = 0
+    for i in list1:
+        if i in list2:
+            list2.remove(i)
             count += 1
             if count == len(string1):
                 print string1, "and", string2, "are anograms."
         else:
             print string1, "and", string2, "are not anograms."
+            return
 main()
