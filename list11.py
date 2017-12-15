77908ebee742022a552ca148c4ac808ef7de30f0
Dylan <dylan@Family-MacBook-Air.local>
2017-12-15 15:11:21 -0500

    First commit

--- list11.py
+++ list11.py
@@ -1,16 +1,19 @@
 #List-11
 #Dylan and Avi
 #12/15/17
-
+def validInput():
+    valid = 1
+    while valid == 1:
+        string1 = raw_input("Enter a string of just letters: ")
+        string2 = raw_input("Enter another string letters: ")
+        for i in string1:
+            if i.isalpha:
+                list1.append(i)
+            else:
+                print "That is not a valid string."
+                return
 def main():
-    string1 = raw_input("Enter a string:")
-    string2 = raw_input("Enter another string: ")
-    list1 = []
-    for i in string1:
-        if i.isalpha:
-            letters.append(i)
-        else:
-            
+    list1 = validInput()
     count = 0
     for i in string1:
         if i in string2:
